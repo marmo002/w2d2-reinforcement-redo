1152047bdaaaea49443cbc16f434fa4cb7e5fa83
marmo002 <marmo_02@hotmail.com>
2018-02-13 11:37:35 -0500

    exercise one of reinforcement

---
+++ exercise1.rb
@@ -0,0 +1,13 @@
+documentary = "The great lions of Africa"
+drama = "The pain of my heart"
+comedy = "Just or laugh"
+dramedy = "Click"
+
+puts "Do you like a good documentary"
+anwser1 = gets.chomp
+
+puts "Do you enjoy a good Drama"
+anwser2 = gets.chomp
+
+puts "Do you preferred a comedy"
+anwser3 = gets.chomp
