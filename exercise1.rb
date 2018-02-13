18d4acd08a1753e4394faf7d79594f7768868c7c
marmo002 <marmo_02@hotmail.com>
2018-02-13 11:52:02 -0500

    wrote code for each user anwser

--- exercise1.rb
+++ exercise1.rb
@@ -1,13 +1,25 @@
-documentary = "The great lions of Africa"
-drama = "The pain of my heart"
+documentary = "The Great Lions"
+drama = "Sister's Keeper"
 comedy = "Just or laugh"
 dramedy = "Click"
 
 puts "Do you like a good documentary"
-anwser1 = gets.chomp
+anwser1 = gets.chomp.downcase
 
 puts "Do you enjoy a good Drama"
-anwser2 = gets.chomp
+anwser2 = gets.chomp.downcase
 
 puts "Do you preferred a comedy"
-anwser3 = gets.chomp
+anwser3 = gets.chomp.downcase
+
+if anwser1 == "yes"
+  "You should watch #{documentary}"
+elsif anwser1 == "no" && ( anwser2 == "yes" && anwser3 == "yes")
+  "You should watch #{dramady}"
+elsif anwser2 == "yes" && (anwser1 == "no" && anwser3 == "no")
+  "You should watch #{drama}"
+elsif anwser3 == "yes" && (anwser1 == "no" && anwser2 == "no")
+  "You should watch #{comedy}"
+else
+  "You should read a book"
+end
